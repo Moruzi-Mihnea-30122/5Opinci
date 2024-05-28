74a0d4467d7b45843301b05c16f5003dbadaa8cb
Mihnea Moruzi <mimoru08@gmail.com>
2024-05-28 09:13:41 +0300

    First commit

---
+++ isp-lab-11-12-master/c1-samples/src/main/java/aut/utcluj/isp/ex1/Drone.java
@@ -0,0 +1,37 @@
+
+package aut.utcluj.isp.ex1;
+
+public class Drone {
+
+    private String id;
+    private boolean state;
+
+    Drone(String dR1, boolean b) {
+        this.id = dR1;
+        this.state = b;
+    }
+
+    Drone(String dR3) {
+        this.id = dR3;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        Drone drone = (Drone) object;
+        return java.util.Objects.equals(id, drone.id);
+    }
+
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id);
+    }
+}
